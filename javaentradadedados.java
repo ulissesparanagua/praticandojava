698fb3b11aad80f387423b0bd9c200fd42e6c52f
Ulisses Paranagua <ulissespnl@hotmail.com>
2023-07-09 12:42:40 -0300

    Criei prática de entrada e saída de dados no java

---
+++ javaentradadedados.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class javaentradadedados {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String x;
+        x = sc.next();
+        System.out.println("Você digitou: " + x);
+
+        sc.close();
+    }
+}
